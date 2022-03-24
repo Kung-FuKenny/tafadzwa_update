dbd5b9ff482ebd425b04c1a182bb44beda6b56c8
KENNY_NGOIE <mukonkolekngoie@gmail.com>
2022-03-24 13:46:59 +0200

    What we do?

---
+++ src/components/features/Features.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import "./Features.css";
+import { BsFillBookmarkStarFill } from "react-icons/bs";
+import phoneFeatures from "../../assets/phone-features.jpg";
+import Feature from "./Feature";
+import { FeatureList } from "./data";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const Features = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
+  return (
+    <section id="features">
+      <div className="container features">
+        <div className="title-2" data-aos="fade-up">
+        <BsFillBookmarkStarFill color="#ffff" size={25} />
+          <h2>What Do We Do?</h2>
+          <p className="u-text-small">
+          Strategy Development, Implementation and Integration Strategy is the scope of an organisation over the long-term: which achieves advantage for the organisation through its configuration of resources to fulfil stakeholder expectations.
+          </p>
+        </div>
+        <div className="features-content">
+          <div className="features-left" data-aos="fade-right">
+            <img src={phoneFeatures} alt="phone" />
+          </div>
+          <div className="features-right" data-aos="fade-left">
+            {FeatureList.map((feature) => (
+              <Feature
+                key={feature.id}
+                icon={feature.icon}
+                heading={feature.heading}
+                text={feature.text}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
--- src/routes/Training.js
+++ src/routes/Training.js
@@ -3,6 +3,7 @@ import Footer from '../components/Footer'
 import HeroImage from '../components/HeroImage'
 import Navbar from '../components/Navbar'
 import TrainingSection from '../components/Training'
+import Features from '../components/features/Features'
 
 const Training = () => {
     return (
@@ -10,6 +11,7 @@ const Training = () => {
             <Navbar />
             <HeroImage heading='About Us' text=' Performance and productivity for our clients' />
             <TrainingSection />
+            <Features/>
             <Footer />
         </div>
     )
